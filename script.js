f29d18b24a995cb74abebbddbabeb46d4bef7bb1
EpLeBoY <115426326+EpLeBoY@users.noreply.github.com>
2022-12-26 18:41:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var randomNum = function(){
+  var random = Math.floor(Math.random()*23);
+  document.getElementById("num").innerHTML =random;
+}
+var socialJS = function(){
+  window.location = "https://epleboy.github.io/EpLeHoUsE/";
+}
+
