cfe0ded9d9fe4133752ddd8e17a097731e67f653
EpLeBoY <115426326+EpLeBoY@users.noreply.github.com>
2023-05-29 18:55:11 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var randomNum = function(){
-  var random = Math.floor(Math.random()*23);
-  document.getElementById("num").innerHTML =random;
-}
-var socialJS = function(){
-  window.location = "https://epleboy.github.io/EpLeHoUsE/";
-}
-
